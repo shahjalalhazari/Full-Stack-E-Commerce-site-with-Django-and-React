a18ecde3aadc55faf60174f1e7f883a00597d0ef
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2024-01-04 23:26:59 +0400

    Passing data from DB to Front-End after serialization.

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,6 +3,9 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
+from .serializers import ProductSerializers
+
 from .products import products
 
 # Create your views here.
@@ -27,14 +30,13 @@ def getRoutes(request):
 
 @api_view(["GET"])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializers(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i["_id"] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializers(product, many=False)
+    return Response(serializer.data)
