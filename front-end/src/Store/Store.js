03cbf5ab800520c17a597b025abd3a229ee5783b
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2024-01-28 23:04:49 +0400

    Cart Reducer, Action and Constants added.

--- front-end/src/Store/Store.js
+++ front-end/src/Store/Store.js
@@ -3,14 +3,22 @@ import { combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { thunk } from "redux-thunk";
 import { productDetailsReducer, productListReducer } from "../reducers/productReducer";
+import { cartReducer } from "../reducers/cartReducer";
 
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+
+const cartItemsFromStorage = localStorage.getItem("cartItems") ?
+  JSON.parse(localStorage.getItem("cartItems")) : []
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage }
+};
 
 const middleware = [thunk]
 
---
+++ front-end/src/actions/cartAction.js
@@ -0,0 +1,21 @@
+import { axios } from 'axios';
+import { CART_ADD_ITEM } from '../constants/cartConstants';
+
+
+export const addToCart = (id, quantity) => async (dispatch, getState) => {
+    const { data } = await axios.get(`http://127.0.0.1:8000/api/products/${id}/`);
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      quantity,
+    }
+  })
+  
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems))
+}
