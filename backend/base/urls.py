223816a473a3bc462e1873833c66e0d07e9c3517
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2024-03-27 21:26:00 +0400

    Simple JWT installed and added to BACKEND project.

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,13 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView
+)
 from django.urls import path
 from . import views
 
 app_name = "base"
 
 urlpatterns = [
+    path('user/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("", views.getRoutes, name="routes"),
     path("products/", views.getProducts, name="products"),
     path("products/<str:pk>/", views.getProduct, name="product"),
