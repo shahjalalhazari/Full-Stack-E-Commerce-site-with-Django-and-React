c87f78da79e5aba871ff121af3afd4c047ea43c1
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2024-03-27 22:07:32 +0400

    Customizing inside token infos and custom info sending with tokens.

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,13 +1,10 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView
-)
 from django.urls import path
 from . import views
 
 app_name = "base"
 
 urlpatterns = [
-    path('user/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("", views.getRoutes, name="routes"),
     path("products/", views.getProducts, name="products"),
     path("products/<str:pk>/", views.getProduct, name="product"),
--- backend/base/views.py
+++ backend/base/views.py
@@ -5,8 +5,41 @@ from rest_framework.response import Response
 
 from .models import Product
 from .serializers import ProductSerializers
-
 from .products import products
+
+
+
+# SIMPLE JET
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    """This is how we can customize info of token inside"""
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super().get_token(user)
+    #     # Add custom claims
+    #     token['username'] = user.username
+    #     token['message'] = "Login Successfully!"
+    #     return token
+
+    """Now this is method where we can customize the sending data"""
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data["username"] = self.user.username
+        data["email"] = self.user.email
+        # these infos comes with "access" & "refresh" token
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+
+
 
 # Create your views here.
 @api_view(["GET"])
